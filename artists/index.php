b4782a68923b12ee491faa76a61af8025fa942bb
brunoluft <193854@upf.br>
2023-11-15 16:51:20 -0300

    atualização gravadora

--- artists/index.php
+++ artists/index.php
@@ -33,7 +33,7 @@
   <div class="row">
     <div class="col mt-5">
       <?php
-        include("config.php");
+        include("../config.php");
         switch(@$_REQUEST["page"]){
           case "novo":
           include("nova-banda-artista.php");
